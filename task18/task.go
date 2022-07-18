b5636d7cc06e66ef2a488743f3040925caa4d8c9
MaksimovMV <m_maksimov@mail.ru>
2022-07-18 20:50:18 +0500

    added solutions to task18

--- task18/task.go
+++ task18/task.go
@@ -3,27 +3,76 @@ package main
 import (
 	"fmt"
 	"sync"
+	"sync/atomic"
 )
 
-type counter struct {
+var implement = make(chan int)
+var read = make(chan int)
+
+// вариант с Mutex
+type mutexCounter struct {
 	count      int
 	sync.Mutex //используется для блокировки одновременного доступа горутинами к структуре
 }
 
+// вариант с atomic
+type atomicCounter struct {
+	count int64
+}
+
+func (c *atomicCounter) Value() int64 {
+	return atomic.LoadInt64(&c.count)
+}
+
+// вариант с управляющей горутиной
+func readCount() int {
+	return <-read
+}
+
+func goroutineCounter() {
+	var count int
+	for {
+		select {
+		case <-implement:
+			count++
+		case read <- count:
+		}
+	}
+}
+
 func main() {
-	c := counter{0, sync.Mutex{}}
+	mutCounter := mutexCounter{0, sync.Mutex{}}
 	wg := sync.WaitGroup{}
 
-	for i := 0; i < 100; i++ {
-		wg.Add(1)
+	atomCounter := atomicCounter{}
 
+	go goroutineCounter()
+
+	for i := 0; i < 100; i++ {
+		wg.Add(3)
+
+		// инкрементируем счетчик 1 с Mutex
 		go func() {
-			c.Lock()
-			c.count++
-			c.Unlock()
-			wg.Done()
+			mutCounter.Lock()
+			defer mutCounter.Unlock()
+			defer wg.Done()
+			mutCounter.count++
+		}()
+
+		// инкрементируем счетчик 2 с atomic
+		go func() {
+			defer wg.Done()
+			atomic.AddInt64(&atomCounter.count, 1)
+		}()
+
+		// инкрементируем счетчик 3 при помощи управляющей горутины
+		go func() {
+			defer wg.Done()
+			implement <- 1
 		}()
 	}
 	wg.Wait()
-	fmt.Println(c.count)
+	fmt.Printf("Счетчик управляемый Mutex: %v\n", mutCounter.count)
+	fmt.Printf("Счетчик управляемый Atomic: %v\n", atomCounter.Value())
+	fmt.Printf("Счетчик управляемый управляющей горутиной: %v\n", readCount())
 }
